3b8f42aca0ff7e717014beffedce477744eb19f1
Shashank Sharma <shashank.ns67@gmail.com>
2021-12-17 00:31:20 +0530

    project  component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,28 @@ import { AppComponent } from './app.component';
 import { BasicsComponent } from './basics/basics.component';
 import { HtmlInTsComponent } from './basics/html-in-ts/html-in-ts.component';  // add the component here in the app module
 
+import { ProjectComponent } from './project/project.component';
+import { HeaderComponent } from './project/components/header/header.component';
+import { ReceipesComponent } from './project/components/receipes/receipes.component';
+import { ReceipeListComponent } from './project/components/receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './project/components/receipes/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './project/components/receipes/receipe-list/receipe-item/receipe-item.component';
+import { ShoppingListComponent } from './project/components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './project/components/shopping-list/shopping-edit/shopping-edit.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     BasicsComponent,
-    HtmlInTsComponent // need to declare the component in this block
+    HtmlInTsComponent, // need to declare the component in this block
+    ProjectComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    ReceipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
