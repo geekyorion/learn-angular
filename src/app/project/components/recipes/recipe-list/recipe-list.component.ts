299184a154c1ab81ffb9c9fbd87c50f10e2cb1c7
Shashank Sharma <shashank.ns67@gmail.com>
2021-12-24 19:40:13 +0530

    worked on following - recipe model is added - test recipe list and detail is added - minor UI improvement

--- src/app/project/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/project/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test', 'https://cdn.pixabay.com/photo/2014/02/01/17/28/apple-256261_960_720.jpg')
+  ];
 
   constructor() { }
 
