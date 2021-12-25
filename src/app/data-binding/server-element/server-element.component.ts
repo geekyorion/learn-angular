6656d855d789b1e9a0ebe394fe76cafd8242746b
Shashank Sharma <shashank.ns67@gmail.com>
2021-12-25 18:49:13 +0530

    view encapsulations

--- src/app/data-binding/server-element/server-element.component.ts
+++ src/app/data-binding/server-element/server-element.component.ts
@@ -1,9 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+
+  // default (it emulates the browser's native Shadow DOM API)
+  encapsulation: ViewEncapsulation.Emulated
+
+  // it remove the css encapsulation
+  // Note: now the styles act globally for other components too
+  // encapsulation: ViewEncapsulation.None
+
+  // it uses the browsers Shadow DOM API to encapsulate CSS styles and inject @shadow-root
+  // NOTE: Global styles don't work in the shodow DOM but the component styles will work
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent implements OnInit {
 
