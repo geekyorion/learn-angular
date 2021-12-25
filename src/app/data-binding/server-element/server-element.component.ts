438360f4e8ce52f873c9827fff30f26b04b3f68a
Shashank Sharma <shashank.ns67@gmail.com>
2021-12-25 18:01:18 +0530

    binding through alias

--- src/app/data-binding/server-element/server-element.component.ts
+++ src/app/data-binding/server-element/server-element.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: { name: string, type: string, content: string } = {
+  // if we pass any string here then we can not bind the data using element property now
+  @Input('serverElement') element: { name: string, type: string, content: string } = {
     name: '', type: '', content: ''
   };
 
