8ad21a6503ed69776381220cf560cbf167ee3a0f
Shashank Sharma <shashank.ns67@gmail.com>
2022-01-22 23:24:05 +0530

    ingredients added to the recipe detail

--- src/app/project/services/recipe.service.ts
+++ src/app/project/services/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../models/ingredient.model";
 import { Recipe } from "../models/recipe.model";
 
 @Injectable({
@@ -6,7 +7,15 @@ import { Recipe } from "../models/recipe.model";
 })
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Test', 'https://cdn.pixabay.com/photo/2014/02/01/17/28/apple-256261_960_720.jpg')
+    new Recipe(
+      'Apple Pie',
+      'Super Tasty Apple Pie',
+      'https://cdn.pixabay.com/photo/2014/02/01/17/28/apple-256261_960_720.jpg',
+      [
+        new Ingredient('Apple', 5),
+        new Ingredient('Cream', 2),
+      ]
+    )
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
