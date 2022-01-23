7ff66a7dc7ace743083c87d12565b081d5142569
Shashank Sharma <shashank.ns67@gmail.com>
2022-01-23 22:53:06 +0530

    dynamic routing

--- src/app/routing/routers.route.ts
+++ src/app/routing/routers.route.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'servers', component: ServersComponent }
+  { path: 'users/:id', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
 ];
