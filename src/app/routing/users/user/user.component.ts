d7b89022ef597623010b33864b54cd6ac5a3634a
Shashank Sharma <shashank.ns67@gmail.com>
2022-02-22 23:30:40 +0530

    params observers

--- src/app/routing/users/user/user.component.ts
+++ src/app/routing/users/user/user.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
 
   user!: { id: number, name: string };
+  paramsSubscription: Subscription | null = null;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,6 +18,19 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
+    }
+    /**
+     * whenver params changes then change is fired from observer
+     */
+    this.paramsSubscription = this.route.params.subscribe((_params: Params) => {
+      this.user.id = _params['id'];
+      this.user.name = _params['name'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.paramsSubscription) {
+      this.paramsSubscription.unsubscribe();
     }
   }
 
