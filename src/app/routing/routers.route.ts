38fc0d838c579f151a320b28efa08a7d315aeaf9
Shashank Sharma <shashank.ns67@gmail.com>
2022-03-16 20:31:22 +0530

    added 404 page

--- src/app/routing/routers.route.ts
+++ src/app/routing/routers.route.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -7,7 +8,7 @@ import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 
 export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   {
     path: 'users',
     component: UsersComponent,
@@ -23,4 +24,6 @@ export const appRoutes: Routes = [
       { path: ':id/edit', component: EditServerComponent }
     ]
   },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
